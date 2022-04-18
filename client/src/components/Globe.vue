0a9fbb6bdfdd07c62246b166d4461a34274f3b93
Leif Gehrmann <leif.gehrmann@gmail.com>
2022-04-18 21:28:36 +0100

    Refactor Colors + Remove unnecessary files

--- client/src/components/Globe.vue
+++ client/src/components/Globe.vue
@@ -1,41 +1,9 @@
 <script lang="ts">
 import * as THREE from 'three';
 import {ArcballControls} from "three/examples/jsm/controls/ArcballControls";
+import { colors } from "../utils/colors"
 
-const colors = [
-  '#6d001a',
-  '#be0039',
-  '#ff4500',
-  '#ffa800',
-  '#ffd635',
-  '#fff8b8',
-  '#00a368',
-  '#00cc78',
-  '#7eed56',
-  '#00756f',
-  '#009eaa',
-  '#00ccc0',
-  '#2450a4',
-  '#3690ea',
-  '#51e9f4',
-  '#493ac1',
-  '#6a5cff',
-  '#94b3ff',
-  '#811e9f',
-  '#b44ac0',
-  '#e4abff',
-  '#de107f',
-  '#ff3881',
-  '#ff99aa',
-  '#6d482f',
-  '#9c6926',
-  '#ffb470',
-  '#000000',
-  '#515252',
-  '#898d90',
-  '#d4d7d9',
-  '#ffffff'
-]
+
 
 var seed = 1;
 function myRandom() {
