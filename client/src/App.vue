ecac51cdc4775b15bc8d1c9b0d484127fc2106b3
Leif Gehrmann <leif.gehrmann@gmail.com>
2022-05-01 20:51:05 +0100

    Refactor increment/decrement buttons into a single component

--- client/src/App.vue
+++ client/src/App.vue
@@ -17,16 +17,13 @@ import {
 } from './utils/state';
 import { initWebSocket, synchronise, requestUpdate } from "./utils/webSocket";
 import RoundButton from "./components/RoundButton.vue";
+import IncrementerButtons from "./components/IncrementerButtons.vue";
 
 let sphereDetail = 224
 let sphereFaceCount = 20 * (sphereDetail + 1) * (sphereDetail + 1)
 let chunkSize = 16875 // See /server/sendmessage/app.js
 
 let showControls = ref(false)
-let rotatingClockwise = ref(false)
-let rotatingAntiClockwise = ref(false)
-let zoomingIn = ref(false)
-let zoomingOut = ref(false)
 let percentLoaded = ref(0)
 let connecting = ref(false)
 let connected = ref(false)
@@ -91,156 +88,106 @@ onMounted(() => {
 
 <template>
   <div class="relative">
-  <Globe
-      :sphere-detail="sphereDetail"
-      :selected-position="selectedPosition"
-      @select-position="selectPosition"
-  />
-  <div
-      class="
-        absolute
-        flex justify-center inset-x-0 text-center pointer-events-none cursor-default
-        transition-all ease-in-out duration-300
-      "
-      :class="{
-        'top-16 sm:top-2': !showControls && (!connected && !connecting),
-        'top-3': !showControls && !(!connected && !connecting),
-        'top-16': showControls,
-      }"
-  >
-    <WebSocketState
-        :connecting="connecting"
-        :connected="connected"
-        :percentLoaded="percentLoaded"
-        @reconnect="startWebSocket"
+    <Globe
+        :sphere-detail="sphereDetail"
+        :selected-position="selectedPosition"
+        @select-position="selectPosition"
     />
-  </div>
-  <div
-      class="absolute top-0 right-0 p-4 select-none pointer-events-none"
-  >
-    <RoundButton
-        @click="showControls = !showControls"
-        :img-src="showControls ? closeImage : menuImage"
-        :label="showControls ? 'Hide menu' : 'Show menu'"
-    />
-  </div>
-  <div
-      class="absolute top-0 w-full overflow-hidden select-none pointer-events-none"
-      style="height: calc(4rem + env(safe-area-inset-top))"
-  >
     <div
         class="
-          w-full h-full
-          p-4
+          absolute
+          flex justify-center inset-x-0 text-center pointer-events-none cursor-default
           transition-all ease-in-out duration-300
-          pointer-events-none
-          flex justify-between gap-2 sm:gap-4 sm:space-x-2
         "
-        :style="{
-          'transform': !showControls ? 'translate(0, calc(-4rem - env(safe-area-inset-top)))' : 'translate(0, 0)',
+        :class="{
+          'top-16 sm:top-2': !showControls && (!connected && !connecting),
+          'top-3': !showControls && !(!connected && !connecting),
+          'top-16': showControls,
         }"
+    >
+      <WebSocketState
+          :connecting="connecting"
+          :connected="connected"
+          :percentLoaded="percentLoaded"
+          @reconnect="startWebSocket"
+      />
+    </div>
+    <div
+        class="absolute top-0 right-0 p-4 select-none pointer-events-none"
     >
       <RoundButton
           @click="showControls = !showControls"
-          label="Information"
-          :img-src="infoImage"
+          :img-src="showControls ? closeImage : menuImage"
+          :label="showControls ? 'Hide menu' : 'Show menu'"
       />
-      <div class="text-neutral-200 backdrop-blur-md grow sm:grow-0 sm:w-40 h-8 rounded-full pointer-events-auto">
-        <button
-            class="bg-neutral-800/70 w-1/2 h-full rounded-l-full"
-            :class="{'bg-neutral-700/70': rotatingAntiClockwise}"
-            @mousedown="rotatingAntiClockwise = true"
-            @mouseup="rotatingAntiClockwise = false"
-            @mouseleave="rotatingAntiClockwise = false"
-        >
-          <img
-              src="./assets/rotate-anti-clockwise.svg"
-              class="w-full h-full p-2 select-none pointer-events-none"
-              alt="Rotate anti-clockwise"
-              style="-webkit-user-drag: none; user-drag: none;"
-          >
-        </button>
-        <button
-            class="bg-neutral-800/70 w-1/2 h-full rounded-r-full"
-            style="box-shadow: -1px 0 0 0 rgba(255, 255, 255, 0.15)"
-            :class="{'bg-neutral-700/70': rotatingClockwise}"
-            @mousedown="rotatingClockwise = true"
-            @mouseup="rotatingClockwise = false"
-            @mouseleave="rotatingClockwise = false"
-        >
-          <img
-              src="./assets/rotate-clockwise.svg"
-              class="w-full h-full p-2 select-none pointer-events-none"
-              alt="Rotate clockwise"
-              style="-webkit-user-drag: none; user-drag: none;"
-          >
-        </button>
-      </div>
-      <div class="text-neutral-200 backdrop-blur-md grow sm:grow-0 sm:w-40 h-8 rounded-full pointer-events-auto">
-        <button
-            class="bg-neutral-800/70 w-1/2 h-full rounded-l-full"
-            :class="{'bg-neutral-700/70': zoomingOut}"
-            @mousedown="zoomingOut = true"
-            @mouseup="zoomingOut = false"
-            @mouseleave="zoomingOut = false"
-        >
-          <img
-              src="./assets/zoom-out.svg"
-              class="w-full h-full p-2 select-none pointer-events-none"
-              alt="Zoom out"
-              style="-webkit-user-drag: none; user-drag: none;"
-          >
-        </button>
-        <button
-            class="bg-neutral-800/70 w-1/2 h-8 rounded-r-full"
-            style="box-shadow: -1px 0 0 0 rgba(255, 255, 255, 0.15)"
-            :class="{'bg-neutral-700/70': zoomingIn}"
-            @mousedown="zoomingIn = true"
-            @mouseup="zoomingIn = false"
-            @mouseleave="zoomingIn = false"
-        >
-          <img
-              src="./assets/zoom-in.svg"
-              class="w-full h-full p-2 select-none pointer-events-none"
-              alt="Zoom in"
-              style="-webkit-user-drag: none; user-drag: none;"
-          >
-        </button>
-      </div>
-      <div class="w-8"></div>
     </div>
-  </div>
-  <div
-      class="absolute bottom-0 h-44 p-4 select-none pointer-events-none overflow-hidden"
-      style="height: calc(11rem + env(safe-area-inset-bottom))"
-  >
-    <RoundButton
-        @click="selectPosition(null)"
-        label="Close color selection"
-        class="transition-all ease-in-out duration-300 transform"
-        :img-src="closeImage"
-        :style="{
-          'transform': selectedPosition === null ? 'translate(0, calc(10rem + env(safe-area-inset-bottom)))' : 'translate(0, 0)',
-        }"
-    />
-  </div>
-  <div
-      class="absolute bottom-0 w-full overflow-hidden pointer-events-none"
-      style="height: calc(7rem + env(safe-area-inset-bottom))"
-  >
     <div
-        class="
-          w-full h-full
-          bg-neutral-800/70 text-neutral-200 backdrop-blur-md
-          transition-all ease-in-out duration-300
-          pointer-events-auto
-        "
-        :style="{
-          'transform': selectedPosition === null ? 'translate(0, calc(7rem + env(safe-area-inset-bottom)))' : 'translate(0, 0)',
-        }"
+        class="absolute top-0 w-full overflow-hidden select-none pointer-events-none"
+        style="height: calc(4rem + env(safe-area-inset-top))"
     >
-      <ColorSelector @select-color-id="updateColorId"/>
+      <div
+          class="
+            w-full h-full
+            p-4
+            transition-all ease-in-out duration-300
+            pointer-events-none
+            flex justify-between gap-2 sm:gap-4 sm:space-x-2
+          "
+          :style="{
+            'transform': !showControls ? 'translate(0, calc(-4rem - env(safe-area-inset-top)))' : 'translate(0, 0)',
+          }"
+      >
+        <RoundButton
+            @click="showControls = !showControls"
+            label="Information"
+            :img-src="infoImage"
+        />
+        <IncrementerButtons
+            left-label="Rotate anti-clockwise"
+            right-label="Rotate clockwise"
+            :left-img-src="rotateAntiClockwiseImage"
+            :right-img-src="rotateClockwiseImage"
+        />
+        <IncrementerButtons
+            left-label="Zoom out"
+            right-label="Zoom in"
+            :left-img-src="zoomOutImage"
+            :right-img-src="zoomInImage"
+        />
+        <div class="w-8"></div>
+      </div>
+    </div>
+    <div
+        class="absolute bottom-0 h-44 p-4 select-none pointer-events-none overflow-hidden"
+        style="height: calc(11rem + env(safe-area-inset-bottom))"
+    >
+      <RoundButton
+          @click="selectPosition(null)"
+          label="Close color selection"
+          class="transition-all ease-in-out duration-300 transform"
+          :img-src="closeImage"
+          :style="{
+            'transform': selectedPosition === null ? 'translate(0, calc(10rem + env(safe-area-inset-bottom)))' : 'translate(0, 0)',
+          }"
+      />
+    </div>
+    <div
+        class="absolute bottom-0 w-full overflow-hidden pointer-events-none"
+        style="height: calc(7rem + env(safe-area-inset-bottom))"
+    >
+      <div
+          class="
+            w-full h-full
+            bg-neutral-800/70 text-neutral-200 backdrop-blur-md
+            transition-all ease-in-out duration-300
+            pointer-events-auto
+          "
+          :style="{
+            'transform': selectedPosition === null ? 'translate(0, calc(7rem + env(safe-area-inset-bottom)))' : 'translate(0, 0)',
+          }"
+      >
+        <ColorSelector @select-color-id="updateColorId"/>
+      </div>
     </div>
   </div>
-</div>
 </template>
---
+++ client/src/components/IncrementerButtons.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="text-neutral-200 backdrop-blur-md grow sm:grow-0 sm:w-40 h-8 rounded-full pointer-events-auto">
+    <button
+        class="bg-neutral-800/70 w-1/2 h-full rounded-l-full"
+        :aria-label="rightLabel"
+        :class="{'bg-neutral-700/70': leftActive}"
+        @mousedown="leftActive = true"
+        @mouseup="leftActive = false"
+        @mouseleave="leftActive = false"
+    >
+      <img
+          aria-hidden="false"
+          class="w-full h-full p-2 select-none pointer-events-none"
+          style="-webkit-user-drag: none;"
+          :src="leftImgSrc"
+          :alt="leftLabel"
+      >
+    </button>
+    <button
+        class="bg-neutral-800/70 w-1/2 h-full rounded-r-full"
+        style="box-shadow: -1px 0 0 0 rgba(255, 255, 255, 0.15)"
+        :aria-label="rightLabel"
+        :class="{'bg-neutral-700/70': rightActive}"
+        @mousedown="rightActive = true"
+        @mouseup="rightActive = false"
+        @mouseleave="rightActive = false"
+    >
+      <img
+          aria-hidden="false"
+          class="w-full h-full p-2 select-none pointer-events-none"
+          style="-webkit-user-drag: none;"
+          :src="rightImgSrc"
+          :alt="rightLabel"
+      >
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  leftLabel: String,
+  leftImgSrc: String,
+  rightLabel: String,
+  rightImgSrc: String,
+})
+
+const leftActive = ref(false)
+const rightActive = ref(false)
+</script>
+
+<style scoped>
+
+</style>
