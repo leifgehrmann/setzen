ab47262265f4d52a917d050c92a7a1a50caec4a0
Leif Gehrmann <leif.gehrmann@gmail.com>
2022-05-14 22:43:08 +0100

    Removed old code from ColorSelector.vue

--- client/src/components/ColorSelector.vue
+++ client/src/components/ColorSelector.vue
@@ -27,24 +27,8 @@
           </svg>
         </div>
       </div>
-      <!--<input v-for="(color, index) in colors"
-           :value="index"
-           v-model="selectedIndex"
-           name="color"
-           type="radio"
-           class="absolute"
-           :style="{
-              width: '20px',
-              top: index % 2 === 0 ? `calc(50% + 5px)` : `calc(50% - 15px)`,
-              left: `calc(${index * 2.31 + 3.3}rem - 10px)`
-           }"
-    >-->
     </span>
   </div>
-<!--    <svg v-if="!up" class="h-10 absolute" viewBox="0 0 100 86.6"><polygon points="0,86.6 100,86.6 50,0" fill="#FFFFFF" @click="log" class="cursor-pointer pointer-events-auto"/></svg>-->
-<!--    <button v-if="up" class="rounded-lg bg-red-200 h-3 w-3 inset-full absolute"></button>-->
-<!--    <button v-if="data" class="rounded-lg bg-red-200 h-3 w-3 inset-full absolute"></button>-->
-
 </template>
 
 <script setup lang="ts">
